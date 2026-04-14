e5a7237c526ce2b18b1857f27d5000e19b565096
MarianaRodriguesS3 <marianamanson@hotmail.com>
2026-04-13 22:02:13 -0300

    alterações feitas para hospedagem do BD

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,38 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
 const app = require('./app');
 const db = require('./database/connection');
 
+const PORT = process.env.PORT || 5000;
+
 (async () => {
   try {
-    await db.query('SELECT 1');
+    await db.query('SELECT NOW()');
     console.log('✅ Banco conectado');
-    app.listen(5000, () => {
-      console.log('🚀 Servidor rodando em http://localhost:5000');
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Servidor rodando na porta ${PORT}`);
     });
+
   } catch (err) {
-    console.error('❌ Falha ao conectar no banco:', err.message);
+    console.error('❌ Falha ao conectar no banco:', err);
   }
 })();
+
+
+
+// const app = require('./app');
+// const db = require('./database/connection');
+
+// (async () => {
+//   try {
+//     await db.query('SELECT 1');
+//     console.log('✅ Banco conectado');
+//     app.listen(5000, () => {
+//       console.log('🚀 Servidor rodando em http://localhost:5000');
+//     });
+//   } catch (err) {
+//     console.error('❌ Falha ao conectar no banco:', err.message);
+//   }
+// })();
