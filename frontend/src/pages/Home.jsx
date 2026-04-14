6f0af39884b2a9e73aff84e66eda4914ce5bcbc7
MarianaRodriguesS3 <marianamanson@hotmail.com>
2026-04-13 23:51:44 -0300

    Migração para PostgreSQL e ajuste seed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import Banner from '../components/Banner';
 import ProductList from '../components/ProductList';
+import api from '../services/api';
 
 function Home() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-  
     async function fetchProducts() {
       try {
-        const response = await fetch('http://localhost:5000/api/products'); //rota do backend
-        const data = await response.json();
-        setProducts(data);
+        const response = await api.get('/products');
+        setProducts(response.data);
       } catch (error) {
         console.error('Erro ao buscar produtos:', error);
       }
@@ -22,8 +21,7 @@ function Home() {
 
   return (
     <>
-      <Banner products={products}/>
-      {/* Aqui o ProductList recebe o array de produtos */}
+      <Banner products={products} />
       <ProductList products={products} />
     </>
   );
