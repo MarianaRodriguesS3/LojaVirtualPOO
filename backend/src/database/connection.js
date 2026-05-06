04bfc76a518b137b35bb0ee6b592b87059151f63
Mariana Rodrigues <166456130+MarianaRodriguesS3@users.noreply.github.com>
2026-05-06 20:32:06 -0300

    Update connection.js

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,26 +1,12 @@
-const { Pool } = require('pg');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
 });
 
 module.exports = pool;
-
-
-
-// const mysql = require('mysql2/promise');
-// require('dotenv').config();
-
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   port: process.env.DB_PORT || 3306,
-// });
-
-// module.exports = pool;
